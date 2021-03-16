8196c1ba122d54da68dee6d935908d3c87f345e5
Govindakumar181 <Govindakumar181@gmail.com>
2021-03-16 13:55:20 +0500

    Contact us Models

--- DropService/src/DropServicing/views.py
+++ DropService/src/DropServicing/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+
+from contact.models import Contact
+
 def index(request):
+
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        name = request.POST["name"]
+        message = request.POST["message"]
+        new_contact = Contact()
+        new_contact.name = name
+        new_contact.message = message
+        new_contact.email = email
+        
+        new_contact.save()
+
+
+
     return render(request, 'index.html',{})
